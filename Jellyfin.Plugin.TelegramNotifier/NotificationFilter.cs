55c8e0cd3b6732bac0d6dcab0efebf635838d061
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2024-04-21 15:52:34 +0300

    enable plugin option

--- Jellyfin.Plugin.TelegramNotifier/NotificationFilter.cs
+++ Jellyfin.Plugin.TelegramNotifier/NotificationFilter.cs
@@ -14,9 +14,13 @@ namespace Jellyfin.Plugin.TelegramNotifier
 
         public async Task Filter(string notificationType, string message)
         {
+            if (Plugin.Config.EnablePlugin == false)
+            {
+                return;
+            }
+
             string botToken = Plugin.Config.BotToken;
             string chatId = Plugin.Config.ChatId;
-
             await _sender.SendMessage(notificationType, message, botToken, chatId).ConfigureAwait(false);
         }
     }
